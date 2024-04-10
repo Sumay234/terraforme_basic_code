5f24a4a000bf9d2687ac8b6000eb483bf56d07f5
Sumay Chatterjee <110117279+Sumay234@users.noreply.github.com>
2024-04-10 09:03:56 +0530

    Add files via upload

---
+++ modules/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value = "ami-080e1f13689e07408"
+instance_type_value = "t2.micro"
+key_name_value = "for-terraform-virginia"
---
+++ modules/variable.tf
@@ -0,0 +1,15 @@
+variable "ami_value" {
+    description = "This the value of AMI "
+#   default = "ami-ahahdd-25457" but here we will use terraform.tfvars
+}
+
+variable "instance_type_value" {
+    description = "This the instance type "
+  
+  
+}
+
+variable "key_name_value" {
+  description = "This is the Key Name OR the key pair value"
+
+}
